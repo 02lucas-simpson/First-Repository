a0a3dae5f6015cafec3c1ec167db6ba8df923c91
Lucas Simspon <02lucas.simpson@gulllakecs.org>
2019-02-11 09:54:52 -0500

    Add a Greetings Function

---
+++ Functions-Git.py
@@ -0,0 +1,11 @@
+# Lucas Simspon
+# 2.6.19
+# 4.13.3: Greating
+
+name = input("What is your name: ")
+
+def greeting():
+    print ("Hi there " + name + "!")
+    print ("Nice to meet you!")
+
+greeting()
