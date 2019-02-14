05bb00bf0da9c82efef8351d5ab243b887c31ff6
Lucas Simspon <02lucas.simpson@gulllakecs.org>
2019-02-14 09:33:55 -0500

    Add Functions and Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -2,6 +2,7 @@
 # 2.6.19
 # 4.13.3: Greating
 
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +10,18 @@ def greeting():
     print ("Nice to meet you!")
 
 greeting()
+
+'''
+#4.13.4: Functions and Variables
+#Lucas Simpson
+#2.14.19
+
+x = 74
+
+def print_something():
+    x = 69
+    print (x)
+
+print_something()
+print(x)
+
